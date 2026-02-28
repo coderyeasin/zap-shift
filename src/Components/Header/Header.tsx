74eda285de1ba3c0161c2f337246c2b32673a9bf
coderyeasin <yeasin3233@gmail.com>
2026-02-28 23:58:44 +0600

    polish

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h3 className="text-xl font-semibold">Zap-Shift Header</h3>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,14 @@
+import Header from "../Header/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to Zap-Shift Home!
+      </h1>
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
