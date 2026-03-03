24341c9e4f3eb113e67c6b87e759f18d23443c48
coderyeasin <yeasin3233@gmail.com>
2026-03-03 17:53:57 +0600

    done-serv

--- src/Components/Services/Services.tsx
+++ src/Components/Services/Services.tsx
@@ -3,27 +3,37 @@ import { serviceCards } from "../../Custom/serviceCards";
 const Services = () => {
   return (
     <section className="container mx-auto w-full  py-20">
-      <div className="h-[1064px]  bg-text-primary rounded-[32px] px-[100px] py-[160px]">
-        <div className="text-center text-white">
-          <h3 className="text-3xl font-bold text-white">Services</h3>
-          <p className="text-light-gray my-3">
+      <div className="h-[1064px]  bg-text-primary rounded-[32px] py-[100px] px-[100px]">
+        <div className="text-center text-white py-5">
+          <h3 className="text-[40px] font-extrabold text-white">
+            Our Services
+          </h3>
+          <p className="font-medium text-light-gray my-3 leading-[26px]">
             Enjoy fast, reliable parcel delivery with real-time tracking and
             zero hassle. From personal packages to <br /> business shipments —
             we deliver on time, every time.
           </p>
         </div>
-        <div className="flex flex-wrap justify-center items-center gap-5">
+
+        <div className="flex flex-wrap justify-center items-center gap-8">
           {serviceCards.map((card) => (
             <div
               key={card.id}
-              className="w-[410px] h-[348px] rounded-[32px] px-[32px] py-[24px] bg-white flex flex-col justify-center items-center gap-4 mt-8"
+              className={`
+                ${card.id === 2 ? "bg-primary " : "bg-white"}
+                  w-[410px] h-[348px] rounded-[32px] px-[32px] py-[24px] flex flex-col justify-center items-center gap-4
+              `}
             >
-              <img src={card.icon} alt={card.title} className="w-12 h-12" />
+              <div className="w-[88px] h-[88px] rounded-full bg-radial-[at_50%_75%] from-[#EEEDFC]/0 to-[#EEEDFC] to-90% flex justify-center items-center">
+                <img src={card.icon} alt={card.title} className="w-12 h-12" />
+              </div>
               <div className="text-center space-y-3">
-                <h4 className="text-xl font-semibold text-text-primary">
+                <h4 className="text-2xl font-bold text-text-primary">
                   {card.title}
                 </h4>
-                <p className="text-gray">{card.description}</p>
+                <p className="text-gray font-medium leading-[26px]">
+                  {card.description}
+                </p>
               </div>
             </div>
           ))}
